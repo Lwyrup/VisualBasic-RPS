5517fc18a7babac580077eace24981c8ec6ac585
Spencer <skgerdes@yahoo.com>
2017-05-31 15:58:50 -0700

    Verbose yet works.

--- Rock, Paper, Scissors/Module1.vb
+++ Rock, Paper, Scissors/Module1.vb
@@ -4,7 +4,9 @@
 
 		Sub Main()
 			Dim game = New Game
-			game.AskForChoice()
+			Dim choice1 As String = game.AskForChoice()
+			Dim choice2 As String = game.AskForChoice()
+			game.DetermineRound(choice1, choice2)
 		End Sub
 
 	End Module
